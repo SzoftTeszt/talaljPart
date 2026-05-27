1de3fbf28f35758617efe8b8962996c05710a8ca
Gyenge Elemér <gyenge@valahol>
2026-05-27 17:42:30 +0200

    getall

--- src/app/base.ts
+++ src/app/base.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Base {
-  
+    private url="https://chat-4b282-default-rtdb.europe-west1.firebasedatabase.app/profiles.json"
+    private http = inject(HttpClient)
+
+    getAll(){
+      return this.http.get(this.url)
+    }
 }
