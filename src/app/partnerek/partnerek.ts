1454bac585c8010ee4c9762ff3594e83be83eeed
Gyenge Elemér <gyenge@valahol>
2026-05-27 17:46:55 +0200

    subscribe

--- src/app/partnerek/partnerek.ts
+++ src/app/partnerek/partnerek.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Base } from '../base';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-partnerek',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './partnerek.html',
   styleUrl: './partnerek.css',
 })
 export class Partnerek {
+  base=inject(Base)
+  tarskeresok:any=[]
 
+  constructor(){
+    this.base.getAll().subscribe(
+      (adatok)=> this.tarskeresok=adatok
+    )
+  }
 }
